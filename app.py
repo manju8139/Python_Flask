b1683e58f019901dbe3b4829fa973575309153ba
Manju <mann.8139@gmail.com>
2023-10-08 14:10:37 +0530

    cookie and flash message added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for,request, render_template
+from flask import Flask, redirect, url_for,request, render_template,make_response,flash
 
 app = Flask(__name__)
 
@@ -29,11 +29,17 @@ def signupcheck():
 @app.route('/user/<name>')
 def user(name):
     if(name == 'admin'):
+        flash("ADMIN LOGIN SUCCESSFUL")
         return redirect(url_for('admin'))
     else:
+        flash("USER LOGIN SUCCESSFUL")
         return redirect(url_for('hello1',user=name))
-    
+
+@app.route('/login1')
+def login1():
+    return render_template('login.html')
 # HTTP Methods route for POST and GET
+
 @app.route('/login',methods = ['POST','GET'])
 def login():
     if request.method == 'POST':
@@ -47,6 +53,28 @@ def login():
 def marks():
     dict = {"P":100,"C":96,"M":100}
     return render_template('userMarks.html',marks = dict)
+
+@app.route('/cookiesignup')
+def csignup():
+    return render_template('setcookie.html')
+
+@app.route('/setcookie',methods=['POST','GET'])
+def setcookie():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        email = request.form['email']
+        res = make_response(render_template('readcookie.html'))
+        res.set_cookie('name',name)
+        res.set_cookie('name',phone)
+        res.set_cookie('name',email)
+        return res
+    
+@app.route('/readcookie')
+def getcookie():
+    name = request.cookies.get('name')
+    return name
+        
 
 if __name__ == '__main__':
     app.run(debug = True)
